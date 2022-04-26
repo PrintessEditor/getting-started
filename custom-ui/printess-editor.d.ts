11bb3c0e2afe9f9e95a3a9ef3cfc9fcb528e53fc
Anja Khan <trulli90@gmail.com>
2022-04-26 15:39:23 +0200

    *bs-layout: - adjusted grid-area for preview display - adjusted snippet-thumb size for layout dialog - adjusted styles for image placement - increased image-filter-snippet size
    *printess.editor.d.ts added:
    - zoomDuration to gotoPreviousPreviewDocument
    - showAlertOnClose to decide if user should confirm closing of editor
    - extended icon list
    
    *uiHelper:
    - made gotoStep asynchronous
    - adjust desktop view for preview to display preview fullwidth
    - skip (done/next/prev) button render if tabs list and showTabNavigation are on
    - added image-placement control, for mobile it is included in scale control
    - resize printess when going to and coming from preview
    - show CloseEditorDialog only if set to true for templates
    - adjusted layout selection dialog and added it for mobile
    - updated ui hints
    - improved display of mobile toolbar
    - select zoom button on mobile when clicking images that are not default images

--- custom-ui/printess-editor.d.ts
+++ custom-ui/printess-editor.d.ts
@@ -496,7 +496,7 @@ export interface iPrintessApi {
   /**
    * If Tab-Navigation is enabled, this method tells if a "PHOTO" tab makes sense.
    */
-   showPhotoTab(): boolean
+  showPhotoTab(): boolean
 
   /**
    * Returns the current form field value and its possible list values if available
@@ -930,7 +930,7 @@ export interface iPrintessApi {
   /**
    * Jumps to the previous available preview document if there is one.
    */
-  gotoPreviousPreviewDocument(): Promise<void>
+  gotoPreviousPreviewDocument(zoomDuration?: number): Promise<void>
 
   /**
    * Jumps to the next available preview document if there is one.
@@ -1105,7 +1105,12 @@ export interface iPrintessApi {
   /**
    * Returns if UI should show a button to enter Expert-Mode
    */
-  hasExpertButton(): boolean,
+  hasExpertButton(): boolean
+
+  /** 
+   * Indicates if UI should show an alert prompt when user attempts to leave the buyer-side 
+   */
+  showAlertOnClose(): boolean
 
   /**
    * Returns an array of buyer-editable documents and a list of frames for each spread including their class-names.
@@ -1760,4 +1765,8 @@ export type iconName =
   | "text-top"
   | "pen-swirl"
   | "circle-1"
-  | "handwriting";
+  | "shirt"
+  | "focus-face"
+  | "focus-group"
+  | "handwriting"
+  | "burger-menu";
